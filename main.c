be0d2dc8a4b01ff623c2aabd9e248ba2403b3f68
Mohamed Emad <107947293+MoEmadd@users.noreply.github.com>
2022-10-23 06:04:45 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,24 +1,50 @@
 #include <stdio.h>
-int set(int , int );
-int clear(int ,int );
-int main()
+void merge(int a[], int m, int b[], int n, int r[]);
+int main(void)
 {
-    int x,s,c,n;
-    printf("Enter any number : ");
-    scanf("%d",&x);
-    printf("Enter the bit you want to set : ");
-    scanf("%d",&s);
-    printf("Enter the bit you want to clear : ");
-    scanf("%d",&c);
-    n=set(x,s);
-    printf("The result = %d",clear(n,c));
-    return 0;
+  int a1[] = {1,3,5,7,9};
+  int a2[] = {2,4,5,6,8};
+  int r[10];
+  merge(a1, 5, a2, 5, r);
+  for (int i = 0; i < 10; i++)
+    printf("r[%d] = %d\n", i, r[i]);
+  return 0;
 }
-int set(int x,int s)
+
+void merge(int a[], int m, int b[], int n, int r[])
 {
-   return x|(1<<s);
-}
-int clear(int n,int c)
-{
-    return n&(~(1<<c));
+  int i = 0, j = 0, k = 0;
+
+  while (i < m && j < n)
+  {
+
+    if (a[i] < b[j])
+    {
+      r[k] = a[i];
+      k++;
+      i++;
+    }
+    else
+    {
+      r[k] = b[j];
+      k++;
+      j++;
+    }
+  }
+
+
+  while (i < m)
+  {
+    r[k] = a[i];
+    k++;
+    i++;
+  }
+
+  while (j < n)
+  {
+    r[k] = b[i];
+    k++;
+    j++;
+  }
+
 }
